58d52a42c8dddd8494c91a187a8d0c1be52e09b5
Chris <Chris@chrismccall.local>
2012-12-09 20:10:45 -0600

    Initial Commit

---
+++ barkprefpane/barkprefpane.h
@@ -0,0 +1,15 @@
+//
+//  barkprefpane.h
+//  barkprefpane
+//
+//  Created by Chris on 12/9/12.
+//  Copyright (c) 2012 ChrisMcCall. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface barkprefpane : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ barkprefpane/barkprefpane.m
@@ -0,0 +1,17 @@
+//
+//  barkprefpane.m
+//  barkprefpane
+//
+//  Created by Chris on 12/9/12.
+//  Copyright (c) 2012 ChrisMcCall. All rights reserved.
+//
+
+#import "barkprefpane.h"
+
+@implementation barkprefpane
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
